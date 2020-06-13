3b81d5f8c118708102300ee11cb9da7549c984c3
Aleksandr Agafonov <alex.2exe2@gmail.com>
2020-06-13 21:55:07 +0500

    add fragment in project

--- app/src/main/java/com/ponchikchik/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/ponchikchik/criminalintent/CrimeFragment.kt
@@ -1,14 +1,18 @@
 package com.ponchikchik.criminalintent
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ponchikchik.criminalintent.data.Crime
+import kotlinx.android.synthetic.main.crime_fragment.*
 
 class CrimeFragment : Fragment() {
     lateinit var crime: Crime
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime()
@@ -23,6 +27,23 @@ class CrimeFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        crime_title.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                crime.title = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        crime_date.text = crime.date.toString()
+        crime_date.isEnabled = false
+
+        crime_solved.setOnCheckedChangeListener { _, isChecked ->
+            crime.isSolved = isChecked
+        }
     }
 
 
