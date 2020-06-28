a8025be7c7d9b16a461ca2ffccf6040f33e4f1fe
Aleksandr Agafonov <alex.2exe2@gmail.com>
2020-06-28 13:57:28 +0500

    add SingleFragmentActivity

--- app/src/main/java/com/ponchikchik/criminalintent/MainActivity.kt
+++ app/src/main/java/com/ponchikchik/criminalintent/MainActivity.kt
@@ -1,17 +1,7 @@
 package com.ponchikchik.criminalintent
 
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, CrimeFragment.newInstance())
-                .commitNow()
-        }
-    }
+class MainActivity : SingleFragmentActivity() {
+    override fun createFragment(): Fragment = CrimeFragment()
 }
---
+++ app/src/main/java/com/ponchikchik/criminalintent/SingleFragmentActivity.kt
@@ -0,0 +1,20 @@
+package com.ponchikchik.criminalintent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+abstract class SingleFragmentActivity : AppCompatActivity() {
+    protected abstract fun createFragment(): Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, createFragment())
+                .commit()
+        }
+    }
+}
